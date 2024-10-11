9bb2135896215816dea542faa6687eee74931ace
windblaser <ranelsova@gmail.com>
2024-10-11 10:45:10 +0300

    пр 2

---
+++ assets/js/alert.js
@@ -0,0 +1,27 @@
+'use strict'
+// alert("Я Java script")
+
+// let name = prompt('Введите имя','Без имени')
+// let surname = prompt('Введите фамилию','Без фамилии')
+// let patronymic = prompt('Введите от','Без от')
+
+// let isTrue = confirm(`Верны ли данные ${name} ${surname}`)
+
+// let a = 'ndwned';
+// let b = 3;
+// let b2 = 3;
+// let sum = b + b2;
+// alert(sum)
+// let string = 'mfdkfmdfkmdmfk';
+// let string2 = string;
+
+// console.log(string,string2)
+
+// let num = 944949;
+// let num2 = 5.5555;
+
+let n = 4;
+let n2 = 6;
+
+let sum = n + n2;
+let
